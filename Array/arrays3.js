ceade67ab064ec5353b5a6b0c60405fbb3e257ae
laresamdeola <74225315+laresamdeola@users.noreply.github.com>
2023-08-13 06:38:07 +0000

    array implementation in js practise

---
+++ Array/arrays3.js
@@ -0,0 +1,46 @@
+class Array {
+  constructor(){
+    this.data = {};
+    this.length = 0
+  }
+
+  // method to get an item
+  access(index) {
+    if(index <= this.length) {
+      return this.data[index];
+    } else {
+      return null;
+    }
+  }
+
+  // method to remove the last item in the array
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  // method to add new item to the array
+  push(value) {
+    this.data[this.length - 1] = value;
+    this.length++;
+    return this;
+  }
+
+  // method to delete an item from the array
+  delete(index) {
+    const item = this.data[index];
+    delete this.data[index];
+    shiftItems(index);
+    this.length--;
+    return item;
+  }
+
+  shiftItems(index) {
+    for(let i = 0; i < this.length; i++) {
+      this.data[i] = this.data[i+1];
+    }
+    delete this.data[this.index - 1];
+  }
+}
--- LinkedList/linkedList.js
+++ LinkedList/linkedList.js
@@ -9,50 +9,48 @@ class LinkedList {
   this.head = null;
   this.size = 0;
 
-  add(element) {
-    var node = new Node(element);
-    var current;
+add(element) {
+  var node = new Node(element);
+  var current;
 
-    if(this.head == null) {
+  if (this.head == null) {
+    this.head = node;
+  } else {
+    current = this.head;
+
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = node;
+  }
+  this.size++;
+}
+
+insertAt(element, index) {
+  if (index < 0 || index > this.size) {
+    throw "Exception: enter a valid index";
+  } else {
+    var node = new Node(element);
+    var curr, prev;
+
+    curr = this.head;
+
+    if (index == 0) {
+      node.next = this.head;
       this.head = node;
     } else {
-      current = this.head;
+      curr = this.head;
+      var it = 0;
 
-      while(current.next) {
-        current = current.next;
+      while (it < index) {
+        it++;
+        prev = curr;
+        curr = curr.next;
       }
-      current.next = node;
+      node.next = curr;
+      prev.next = node;
     }
     this.size++;
   }
-
-  insertAt(element, index) {
-    if(index < 0 || index > this.size) {
-      throw "Exception: enter a valid index";
-    } else {
-      var node = new Node(element);
-      var curr, prev;
-
-      curr = this.head;
-
-      if(index == 0) {
-        node.next = this.head;
-        this.head = node;
-      } else {
-        curr = this.head;
-        var it = 0;
-
-        while(it < index) {
-          it++;
-          prev = curr;
-          curr = curr.next;
-        }
-        node.next = curr;
-        prev.next = node;
-      }
-      this.size++;
-    }
-  }
-
-  
+}
 }
