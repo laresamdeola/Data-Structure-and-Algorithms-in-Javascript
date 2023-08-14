38f21adf3ff3faff3305ad8bd1ca5a2b3ae3c74b
laresamdeola <74225315+laresamdeola@users.noreply.github.com>
2023-08-14 12:52:30 +0000

    implement shift method

---
+++ Array/arrays6.js
@@ -0,0 +1,45 @@
+class Array {
+  constructor() {
+    this.data = {};
+    this.length = 0;
+  }
+
+  // method to access the items in the array
+  get(index) {
+    if(index <= this.length) {
+      return this.data[index];
+    } else {
+      throw "Exception: Index out of bounds";
+    }
+  }
+
+  // method to remove the last element in the array
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return this;
+  }
+
+  //method to add an element to the array
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this;
+  }
+
+  // method to delete an item from the array
+  delete(index) {
+    delete this.data[index];
+    shiftItems(index);
+    this.length--;
+    return this;
+  }
+
+  shiftItems(index) {
+    for (let i = 0; i < this.length; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+  }
+}
---
+++ Array/arrays7.js
@@ -0,0 +1,45 @@
+class Array {
+  constructor() {
+    this.data = {};
+    this.length = 0;
+  }
+
+  //method to access elements in the array
+  get(index) {
+    if(index <= this.length) {
+      return this.data[index];
+    } else {
+      return "Index out of range. Enter a value lower or equal to the length of the array";
+    }
+  }
+
+  // method to remove the last item
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return this;
+  }
+
+  // method to add an item to the last of the array
+  push(value) {
+    this.data[this.length] = value;
+    this.length++;
+    return this;
+  }
+
+  // method to add elements to the beginning of the array
+  shift(value) {
+    this.data[0] = value;
+    shiftItems(value);
+    this.length++;
+    return this;
+  }
+
+  // to shift array to the left
+  shiftItems(value) {
+    for(let i = 0; i < this.length; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+  }
+}
